8f3d3a033e1f0bc4906b95814c867467c541484c
BeezMaster <gwjvanhattem@gmail.com>
2025-04-06 20:53:22 +0200

    WIP

---
+++ _ide_helper_models.php
@@ -0,0 +1,88 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ *
+ *
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ * @method static \Illuminate\Database\Eloquent\Builder isFetched()
+ * @property int $id
+ * @property int $github_id
+ * @property string|null $url
+ * @property string|null $html_url
+ * @property string|null $location
+ * @property string|null $hireable
+ * @property string|null $name
+ * @property string|null $email
+ * @property string|null $twitter_username
+ * @property string|null $blog
+ * @property bool $is_done
+ * @property bool $is_fetched
+ * @property array<array-key, mixed>|null $linkedin_links
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \App\Clients\GitHubClient\Enums\UserTypesEnum $type
+ * @method static \Database\Factories\ProfileFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereBlog($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereGithubId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereHireable($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereHtmlUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereIsDone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereIsFetched($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereLinkedinLinks($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereTwitterUsername($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Profile whereUrl($value)
+ */
+	class Profile extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ *
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
